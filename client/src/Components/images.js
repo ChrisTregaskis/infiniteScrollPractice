51a0511317f5677c2dab00fccf908f72205456a8
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-05-14 20:16:51 +0100

    created skeleton class component for Images

---
+++ client/src/Components/images.js
@@ -0,0 +1,19 @@
+import React from "react";
+import axios from 'axios';
+import InfiniteScroll from "react-infinite-scroll-component";
+
+class Images extends React.Component {
+    state = {
+
+    };
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Images;
