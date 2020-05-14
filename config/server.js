054897bd7d64b5a1b8028137b6f2b2bf7e560a1e
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-05-14 19:37:54 +0100

    removed authentication for pushing to git

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = {
     APPLICATION_ID: process.env.APPLICATION_ID ||
-    'WU8qURsCzKMwIh-tjrZ04k9jNEwEZe0IREqFJbIPpf8',
+    'REPLACE',
     SECRET: process.env.SECRET ||
-    'EZj_5udd9phYkbJZp9pm--ISajS0KMy4gZuL74rIQUI',
+    'REPLACE',
     CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
 };
