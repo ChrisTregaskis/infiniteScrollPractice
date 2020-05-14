0f16135e80103af93cb196ebc60fbfd4018fd677
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-05-14 20:25:45 +0100

    initial draft from images js with componentDidMount axios call and state set

--- client/src/Components/images.js
+++ client/src/Components/images.js
@@ -4,13 +4,23 @@ import InfiniteScroll from "react-infinite-scroll-component";
 
 class Images extends React.Component {
     state = {
-
+        images: [],
+        count: 30,
+        start: 1
     };
 
+    componentDidMount() {
+        const { count, start } = this.state
+        axios
+            .get(`/api/photos?count=${count}&start=${start}`)
+            .then(res => this.setState({ images: res.data }))
+    }
+
     render() {
+        console.log(this.state)
         return (
             <div>
-
+                Hello
             </div>
         );
     }
